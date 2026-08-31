c8ddb59f9959f65db8866ffac17f487c1f0ab31d
slfubu <thapanutputtathakorn@gmail.com>
2026-09-01 03:38:21 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.UBU_APP_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbywe0JyIknMFCm8Z_fXwzhpA2Xqgk64rOIoGsSJdcwwq_AGAEf1idMnMW9tfCzFqMMb/exec',
-  REQUEST_TIMEOUT_MS: 120000
-});
