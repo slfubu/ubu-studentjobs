21af96868f8edf4f6c0be86032edc86e099fabbe
slfubu <thapanutputtathakorn@gmail.com>
2026-09-01 03:00:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.UBU_APP_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbywe0JyIknMFCm8Z_fXwzhpA2Xqgk64rOIoGsSJdcwwq_AGAEf1idMnMW9tfCzFqMMb/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxu4r1cOKJR9Sf8PCh-6EaFOV6KNiotNTUXat3momVVUgiSwDzDwc9kmBOL8tLJIE3PdA/exec',
   REQUEST_TIMEOUT_MS: 120000
 });
