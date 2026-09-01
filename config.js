515ad943e1123e7707e02a702f63a17e6184d420
slfubu <thapanutputtathakorn@gmail.com>
2026-09-02 05:07:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const config = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbz4ykKdwE_2K-GEkwqJWnf1yftEwh0zwDe9Tc8gDXOsjjJXrvBTCtUOj080y2rCbMw2/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbw5VxGqIOhu1gUwrlWKnMRaIxBVBttSQ4wSyf__C-mfRzVjP9iJ0r8Fub7JDRq2o9kG/exec',
     REQUEST_TIMEOUT_MS: 60000,
     DEBUG_API: false
   });
